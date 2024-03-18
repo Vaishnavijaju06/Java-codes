fe0f0c0c0305dee0e6907181b4975518bb69d6e9
Vaishnavijaju06 <163777144+Vaishnavijaju06@users.noreply.github.com>
2024-03-18 19:33:54 +0530

    Add files via upload

---
+++ TKAcalculator.java
@@ -0,0 +1,7 @@
+
+public class TKAcalculator {
+
+	void AddTwoNum( int n1, int n2) {
+
+	}	
+}
---
+++ testCalculator.java
@@ -0,0 +1,7 @@
+
+public class testCalculator {
+public static void main(String[] args) {
+	TKAcalculator c1 = new TKAcalculator();
+	System.out.println(c1.AddTwoNum (3, 4));
+}
+}
